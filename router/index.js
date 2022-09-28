602c4e00cf5c28acea9f0e170d59d45c28098942
bong-u <bongudev@gmail.com>
2022-09-28 06:53:55 +0000

    Render view.ejs

--- router/index.js
+++ router/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello router');
+    res.render('view')
 });
 
 module.exports = router;
