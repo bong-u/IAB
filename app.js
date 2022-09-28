13c1f35dd65a18dcc528008a6af71aab3b805d23
bong-u <bongudev@gmail.com>
2022-09-28 07:20:27 +0000

    Set login router

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
 
 const server = http.createServer(app);
 const PORT = 8181;
 
-const indexRouter = require('./router/index');
+const loginRouter = require('./router/login');
 
 server.listen(PORT, function() {
     console.log('Sever running on :', PORT)
 });
 
-app.use('/index', indexRouter);
+app.use('/', loginRouter);
--- router/login.js
+++ router/login.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('view')
+router.get('/login', (req, res) => {
+    res.render('login')
 });
 
 module.exports = router;
