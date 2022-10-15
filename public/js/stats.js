4d0d15f9e71380976d563a519f2c6f3283ab82fb
bong-u <bongudev@gmail.com>
2022-10-15 15:09:59 +0000

    Modify design : element align

--- public/js/stats.js
+++ public/js/stats.js
@@ -19,32 +19,21 @@ const switch_stats = (event) => {
 btn_group.addEventListener('click', switch_stats);
 
 
-const mychart = document.getElementById('stats_chart');
+const stats_chart = document.getElementById('stats_chart').getContext('2d');
 
 const data = {
+    labels: ['술값','식비','생활비'],
     datasets: [{
         data: [10, 20, 30],
         backgroundColor: ['#CCF3EE', '#FFD1D1', '#C8DBBE'],
     }],
-    labels: ['술값','식비','생활비'],
-    options: {
-        plugins: {
-            legend: {
-                labels: {
-                    // This more specific font property overrides the global property
-                    font: {
-                        size: 16,
-                        family: "'Malang'",
-                    }
-                }
-            }
-        }
-    },
 };
 
-Chart.defaults.global.defaultFontColor = "#000000";
+Chart.defaults.global.defaultFontColor = '#000000';
 Chart.defaults.global.defaultFontFamily = 'Malang';
-const chart = new Chart(mychart, {
+console.log(Chart.defaults);
+Chart.defaults.global.defaultFontSize = 16;
+const chart = new Chart(stats_chart, {
     type: 'pie',
     data: data,
     options: {}
