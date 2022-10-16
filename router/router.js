ad0ed3e49317b2120cb3bcf0c95a20f1d3b593ed
bong-u <bongudev@gmail.com>
2022-10-16 15:33:27 +0000

    Use controller : getAsset()

--- router/router.js
+++ router/router.js
@@ -1,14 +1,7 @@
 const express = require('express');
+const ct = require('../controllers/controller.js');
+
 const router = express.Router();
-const asset_list = [
-    { name: "Asset1", money: "1000",    type: "card",    color: "#FFD1D1" },
-    { name: "Asset2", money: "2000",    type: "account", color: "#F9F7CF"},
-    { name: "Asset3", money: "3000000", type: "payment", color: "#CCF3EE"},
-    { name: "Asset4", money: "40000",   type: "card",    color: "#C8DBBE"},
-    { name: "Asset5", money: "15000",   type: "account", color: "#9ADCFF"},
-    { name: "Asset6", money: "250",     type: "payment", color: "#F0D9FF"
-    },
-];
 const expense_type = ['식비', '교통비', '공과금', '술'];
 
 router.get('/login', (req, res) => {
@@ -17,9 +10,7 @@ router.get('/login', (req, res) => {
 router.get('/', (req, res) => {
     res.render('index', { routerName : 'home'} );
 });
-router.get('/asset', (req, res) => {
-    res.render('index', { routerName : 'asset', asset_list : asset_list} );
-});
+router.get('/asset', ct.getAsset);
 router.get('/stats', (req, res) => {
     res.render('index', { routerName : 'stats'} );
 });
