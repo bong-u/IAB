07028e525670ce8a4860ac83c9af08ad46ce1957
bong-u <bongudev@gmail.com>
2022-10-16 04:42:16 +0000

    Add send asset_list to /add_item

--- router/router.js
+++ router/router.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const asset_list = [
+    { name: "Asset1", money: "1000",    type: "card",    color: "#FFD1D1" },
+    { name: "Asset2", money: "2000",    type: "account", color: "#F9F7CF"},
+    { name: "Asset3", money: "3000000", type: "payment", color: "#CCF3EE"},
+    { name: "Asset4", money: "40000",   type: "card",    color: "#C8DBBE"},
+    { name: "Asset5", money: "15000",   type: "account", color: "#9ADCFF"},
+    { name: "Asset6", money: "250",     type: "payment", color: "#F0D9FF"
+    },
+];
 
 router.get('/login', (req, res) => {
     res.render('login');
@@ -8,51 +17,13 @@ router.get('/', (req, res) => {
     res.render('index', { routerName : 'home'} );
 });
 router.get('/asset', (req, res) => {
-    asset_list = [
-        { 
-            name: "Asset1",
-            money: "1000",
-            type: "card",
-            color: "#FFD1D1"
-        },
-        { 
-            name: "Asset2",
-            money: "2000",
-            type: "account",
-            color: "#F9F7CF"
-        },
-        { 
-            name: "Asset3",
-            money: "3000000",
-            type: "payment",
-            color: "#CCF3EE"
-        },
-        { 
-            name: "Asset4",
-            money: "40000",
-            type: "card",
-            color: "#C8DBBE"
-        },
-        { 
-            name: "Asset5",
-            money: "15000",
-            type: "account",
-            color: "#9ADCFF"
-        },
-        { 
-            name: "Asset6",
-            money: "250",
-            type: "payment",
-            color: "#F0D9FF"
-        },
-    ]
     res.render('index', { routerName : 'asset', asset_list : asset_list} );
 });
 router.get('/stats', (req, res) => {
     res.render('index', { routerName : 'stats'} );
 });
 router.get('/add_item', (req, res) => {
-    res.render('index', { routerName : 'add_item'} );
+    res.render('index', { routerName : 'add_item', asset_list : asset_list} );
 });
 
 module.exports = router;
