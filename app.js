fc8e370d13a2cffbf8a81fafe9db977d8eca6595
bong-u <bongudev@gmail.com>
2022-10-31 16:21:01 +0000

    Add react framework to project

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const http = require('http')
-const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.use(express.static(path.join(__dirname, 'iab/build')));
 
+app.get('/', function(req, res)  {
+    res.sendFile(path.join(__dirname, 'iab/build/index.html'));
+});
 
 const server = http.createServer(app);
 const PORT = 8001;
 
-const router = require('./router/router');
-
 server.listen(PORT, function() {
     console.log('Sever running on :', PORT)
 });
-
-app.use('/', router);
