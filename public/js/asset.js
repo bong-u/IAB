b9a203981ebf40e1ec4e038ed0b5d6fb4e63c91e
bong-u <bongudev@gmail.com>
2022-10-31 15:50:50 +0000

    Implement add_asset()

--- public/js/asset.js
+++ public/js/asset.js
@@ -4,6 +4,18 @@ const open_modal_btn = document.getElementById('open_modal_btn');
 const close_modal_btn = document.getElementById('close_modal_btn');
 const modal = document.getElementById('add_asset_modal');
 
+const add_asset_form = document.getElementById('add_asset_form');
+const input_money = document.getElementById('input_money');
+
+const add_asset = (e) => {
+    // e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = {};
+    formData.forEach((value, key) => (data[key] = value));
+
+    console.log (data);
+};
+
 open_modal_btn.addEventListener('click', () => {
     modal.classList.add('show');
     modal.style.display = 'block';
@@ -11,4 +23,14 @@ open_modal_btn.addEventListener('click', () => {
 close_modal_btn.addEventListener('click', () => {
     modal.classList.remove('show');
     modal.style.display = 'none';
+    add_asset_form.reset();
 });
+input_money.addEventListener('focusout', (e) => {
+    let value = e.target.value;
+    input_money.value = value.replace (/[^0-9]/g, '') + '원';
+});
+input_money.addEventListener('keyup', (e) => {
+    let value = e.target.value;
+    input_money.value = value.replace (/[^0-9]/g, '');
+});
+add_asset_form.addEventListener('submit', add_asset);
