0b376d51c5394cd6cd3b2f6d49296695e5c9bab6
bong-u <bongudev@gmail.com>
2022-11-03 17:24:45 +0000

    Rename "post/asset" to "post/"

--- router/router.js
+++ router/router.js
@@ -8,17 +8,18 @@ router.get('/', function(req, res)  {
     res.sendFile(path.join(__dirname, 'iab/build/index.html'));
 });
 
-router.get('/asset', (req, res) => {
-    res.render('index');
-});
-
-router.post('/asset', async (req, res) => {
+router.post('/', async (req, res) => {
     const data = {
         asset_color_list : ct.ASSET_COLOR,
         asset_list : await ct.getAsset()
     };
     res.send(data);
 });
+
+router.get('/asset', (req, res) => {
+    res.render('index');
+});
+
 
 router.get('/stats', (req, res) => {
     res.render('index', { routerName : 'stats'} );
