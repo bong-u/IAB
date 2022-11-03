4496b4564d2ad7fce0303561ac50ba9e60075d9e
bong-u <bongudev@gmail.com>
2022-11-03 15:26:11 +0000

    Modify trifling matter

--- iab/src/App.js
+++ iab/src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css'
 import TopNav from './components/TopNav';
 import SideNav from './components/SideNav';
 import Home from './components/Home';
 import Asset from './components/Asset';
 import Stats from './components/Stats';
 import Add from './components/Add';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
-function App() {
+const App = () => {
   return (
     <div  className="d-flex flex-column flex-nowrap h-100">
       <BrowserRouter>
