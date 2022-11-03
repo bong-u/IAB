2f11f949bf084b6107c152dafe21955ed6b619e8
bong-u <bongudev@gmail.com>
2022-11-03 15:27:09 +0000

    Fix router /asset for react

--- router/router.js
+++ router/router.js
@@ -4,34 +4,25 @@ const ct = require('../controllers/controller.js');
 const router = express.Router();
 const expense_type = ['식비', '교통비', '공과금', '술'];
 
-router.get('/login', (req, res) => {
-    res.render('login');
+router.get('/', function(req, res)  {
+    res.sendFile(path.join(__dirname, 'iab/build/index.html'));
 });
-router.get('/', (req, res) => {
-    res.render('index', { routerName : 'home'} );
+
+router.get('/asset', (req, res) => {
+    res.render('index');
 });
-router.get('/asset', async (req, res) => {
-    const asset_list = await ct.getAsset();
-    res.render('index', {
-        routerName : 'asset',
-        asset_color : ct.ASSET_COLOR,
-        asset_list : asset_list
-    });
-});
+
 router.post('/asset', async (req, res) => {
-    const asset_list = await ct.addAsset(req.body);
-    return;
+    const data = {
+        asset_color_list : ct.ASSET_COLOR,
+        asset_list : await ct.getAsset()
+    };
+    res.send(data);
 });
+
 router.get('/stats', (req, res) => {
     res.render('index', { routerName : 'stats'} );
 });
-router.get('/add', async (req, res) => {
-    const asset_list = await ct.getAsset();
-    res.render('index', {
-        routerName : 'add',
-        asset_list : asset_list,
-        expense_type : expense_type,
-    } );
-});
+
 
 module.exports = router;
