31e46ba8c8c13a238d12cbe5d18ac7911ce0b6a5
bong-u <bongudev@gmail.com>
2022-11-04 04:25:39 +0000

    New router main

--- iab/src/App.js
+++ iab/src/App.js
@@ -1,46 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useEffect, useState } from 'react';
-import TopNav from './components/TopNav';
-import SideNav from './components/SideNav';
-import Home from './components/Home';
-import Asset from './components/Asset';
-import Stats from './components/Stats';
-import Add from './components/Add';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import Main from './components/Main';
 
 const App = () => {
-  const [assetList, setAssetList] = useState([]);
-  const [assetColorList, setAssetColorList] = useState([]);
-  const [expenseTypeList, setExpenseTypeList] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3001', { method: 'POST' })
-      .then(res => res.json())
-      .then(data => {
-        setAssetList(data['asset_list']);
-        setAssetColorList(data['asset_color_list']);
-        setExpenseTypeList(data['expense_type_list']);
-      });
-  }, []);
-
   return (
-    <div  className="d-flex flex-column flex-nowrap h-100">
+    <div className="h-100">
       <BrowserRouter>
-        <TopNav></TopNav>
-        <main className="d-flex h-100">
-          <aside id="sidebar" className="col-md-3 col-lg-3 col-xl-2 d-md-block bg-light sidebar collapse px-3 border-end">
-            <SideNav></SideNav>
-          </aside>
-          <section className="col-sm-12 col-md-9 col-lg-9 col-xl-10 p-5">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/asset" element={<Asset assetList={assetList} assetColorList={assetColorList}/>} />
-              <Route path="/stats" element={<Stats />} />
-              <Route path="/add" element={<Add assetList={assetList} expenseTypeList={expenseTypeList}/>} />
-            </Routes>
-          </section>
-        </main>
+        <Routes>
+          <Route path="/*" element={<Main />}>
+          </Route>
+          {/* <Route path="/login" element={}/> */}
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ iab/src/components/Main.js
@@ -0,0 +1,47 @@
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import TopNav from './TopNav';
+import SideNav from './SideNav';
+import Home from './Home';
+import Asset from './Asset';
+import Stats from './Stats';
+import Add from './Add';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../App.css'
+
+const Main = () => {
+  const [assetList, setAssetList] = useState([]);
+  const [assetColorList, setAssetColorList] = useState([]);
+  const [expenseTypeList, setExpenseTypeList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001', { method: 'POST' })
+      .then(res => res.json())
+      .then(data => {
+        setAssetList(data['asset_list']);
+        setAssetColorList(data['asset_color_list']);
+        setExpenseTypeList(data['expense_type_list']);
+      });
+  }, []);
+
+  return (
+    <div className="d-flex flex-column flex-nowrap h-100">
+        <TopNav></TopNav>
+        <main className="d-flex h-100">
+          <aside id="sidebar" className="col-md-3 col-lg-3 col-xl-2 d-md-block bg-light sidebar collapse px-3 border-end">
+            <SideNav></SideNav>
+          </aside>
+          <section className="col-sm-12 col-md-9 col-lg-9 col-xl-10 p-5">
+            <Routes>
+              <Route path="" element={<Home />} />
+              <Route path="asset" element={<Asset assetList={assetList} assetColorList={assetColorList}/>} />
+              <Route path="stats" element={<Stats />} />
+              <Route path="add" element={<Add assetList={assetList} expenseTypeList={expenseTypeList}/>} />
+            </Routes>
+          </section>
+        </main>
+    </div>
+  );
+}
+
+export default Main;
