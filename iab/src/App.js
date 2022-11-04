ec9b9f0c29826aebd4ed49cbf045a3fa2b0e2321
bong-u <bongudev@gmail.com>
2022-11-04 04:39:48 +0000

    Implement login UI

--- iab/src/App.js
+++ iab/src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from './components/Main';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <div className="h-100">
       <BrowserRouter>
         <Routes>
-          <Route path="/*" element={<Main />}>
+          <Route path="/*" element={<Main/>}>
           </Route>
-          {/* <Route path="/login" element={}/> */}
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ iab/src/components/Login.js
@@ -0,0 +1,32 @@
+const Login = () => {
+  return (
+  <div className="py-5 my-5">
+      <header className="py-5 text-center">
+        <h1>한눈에가계부</h1>
+      </header>
+      <section className="d-flex justify-content-center">
+        <form id="loginForm" action="/">
+          <div className="my-4 form-floating">
+            <input type="email" className="form-control" id="floatingEmail" placeholder="email" />
+            <label>Email</label>
+          </div>
+          <div className="my-4 form-floating">
+            <input type="password" className="form-control" id="floatingPw" placeholder="password" />
+            <label >Password</label>
+          </div>
+
+          <div className="d-flex justify-content-between gap-3">
+            <span>
+              <input type="button" className="w-100 px-5 py-2 btn btn-lg btn-dark" value="Signup" />
+            </span>
+            <span>
+              <input type="submit" className="w-100 px-5 py-2 btn btn-lg btn-dark" value="Login" />
+            </span>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
+}
+
+export default Login;
