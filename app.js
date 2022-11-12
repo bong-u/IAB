da6de2432cdab05788fc295abd8dfdcc63087b16
bong-u <bongudev@gmail.com>
2022-11-12 16:04:10 +0000

    Use dotenv

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const http = require('http')
 const path = require('path');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'iab/build')));
 app.use(cors({
     origin: ['http://localhost:3000'],
     credentials: true
-}))
+}));
 app.use(express.json());
+dotenv.config();
 
 const router = require('./router/router');
 
