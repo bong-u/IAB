62d5e026a849d8b4020fdb89cec91425197b3d7d
bong-u <bongudev@gmail.com>
2022-11-16 03:35:23 +0000

    Use settings.ini

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import uvicorn
 from fastapi import FastAPI, Request
 # from fastapi.middleware.cors import CORSMiddleware
 
-# from decouple import config
+from decouple import config
 # from backend.routers import rest
 
-# isDebug = config('DEBUG', cast=bool)
-isDebug = True
+isDebug = config('DEBUG', cast=bool)
 
 app = FastAPI()
 
@@ -19,4 +18,4 @@ def render(request: Request):
     return 'it works!'
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8001)
+    uvicorn.run('main:app', host='0.0.0.0', port=8001, debug=isDebug)
